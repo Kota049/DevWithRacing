ec1bce84fe25cf340dd54fafa2a5cf3958cb5b6a
Kota <yuzukh0211@gmail.com>
2024-06-29 01:12:24 +0900

    modify HorseCard to draggable

--- front/src/app/components/atomic/HorseCard.tsx
+++ front/src/app/components/atomic/HorseCard.tsx
@@ -1,25 +1,33 @@
-import { Card, CardHeader, Avatar, IconButton } from "@mui/material";
+"use client";
+import { useDraggable } from "@dnd-kit/core";
+import { Card, CardHeader, Avatar } from "@mui/material";
 import { red } from "@mui/material/colors";
 
 interface HorseCardArg {
   order: number;
   name: string;
   jockey: string;
+  id: string;
 }
 
-const HorseCard = ({ order, name, jockey }: HorseCardArg) => {
+const HorseCard = ({ order, name, jockey, id }: HorseCardArg) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: id,
+  });
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="avatar">
-            {order}
-          </Avatar>
-        }
-        title={name}
-        subheader={jockey}
-      />
-    </Card>
+    <div ref={setNodeRef} {...listeners} {...attributes}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="avatar">
+              {order}
+            </Avatar>
+          }
+          title={name}
+          subheader={jockey}
+        />
+      </Card>
+    </div>
   );
 };
 
--- front/src/app/components/page.tsx
+++ front/src/app/components/page.tsx
@@ -1,8 +1,9 @@
 import { NextPage } from "next";
-import Header from "./molecules/header";
 import HorseCard from "./atomic/HorseCard";
 
 const DebugPage: NextPage = () => {
-  return <HorseCard order={0} name={"アルナシーム"} jockey={"鮫島克"} />;
+  return (
+    <HorseCard order={0} name={"アルナシーム"} jockey={"鮫島克"} id={""} />
+  );
 };
 export default DebugPage;
