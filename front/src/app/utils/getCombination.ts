2bea7200422d42258f6c78ed40e93bfbe8ce01b4
Kota <yuzukh0211@gmail.com>
2024-06-30 21:00:20 +0900

    [Green] case has duplicated in f & l

--- front/src/app/utils/getCombination.ts
+++ front/src/app/utils/getCombination.ts
@@ -8,6 +8,7 @@ const getCombination = ({ f, s, l }: getCombinationArg): number[][] => {
   let res = [];
   for (const i1 of f) {
     s = s.filter((el) => el != i1);
+    l = l.filter((el) => el != i1);
     for (const i2 of s) {
       for (const i3 of l) {
         res.push([i1, i2, i3]);
