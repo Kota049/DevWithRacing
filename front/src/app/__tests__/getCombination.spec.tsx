bb64b6c8e4a3b3d8582613b48de5f885f332d3c8
Kota <yuzukh0211@gmail.com>
2024-06-30 20:52:27 +0900

    fix test for contain test

--- front/src/app/__tests__/getCombination.spec.tsx
+++ front/src/app/__tests__/getCombination.spec.tsx
@@ -7,8 +7,16 @@ describe("getCombination", () => {
     expect(res).toStrictEqual([]);
   });
   it("case 1 pattern", () => {
-    const arg = [[1], [1], [1]];
+    const arg = [[1], [2], [3]];
     const res = getCombination({ f: arg[0], s: arg[1], l: arg[2] });
-    expect(res).toStrictEqual([[1, 1, 1]]);
+    expect(res).toContainEqual([1, 2, 3]);
   });
+  // it("case 2patern", () => {
+  //   const arg = [[1], [2, 3], [4]];
+  //   const res = getCombination({ f: arg[0], s: arg[1], l: arg[2] });
+  //   expect(res).toStrictEqual([
+  //     [1, 2, 4],
+  //     [1, 3, 4],
+  //   ]);
+  // });
 });
