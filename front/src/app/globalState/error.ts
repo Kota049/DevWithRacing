e01f69ba7f23420ab9fa4aebd32ee77886aa9db3
Kota <yuzukh0211@gmail.com>
2024-07-01 03:52:40 +0900

    add error snack bar & add favicon

---
+++ front/src/app/globalState/error.ts
@@ -0,0 +1,6 @@
+"use client";
+import { atom } from "jotai";
+
+const errorAtom = atom([]);
+
+export default errorAtom;
---
+++ front/src/app/race/layout.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "レース情報",
+  description: "Select Horse",
+};
