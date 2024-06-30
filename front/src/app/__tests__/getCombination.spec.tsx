9442ba79f6126b66a5e6dd1d5a9a981329def868
Kota <yuzukh0211@gmail.com>
2024-06-30 21:10:29 +0900

    [Red] case has duplicated in s & s

--- front/src/app/__tests__/getCombination.spec.tsx
+++ front/src/app/__tests__/getCombination.spec.tsx
@@ -35,4 +35,10 @@ describe("getCombination", () => {
     expect(res).toContainEqual([1, 2, 3]);
     expect(res.length).toStrictEqual(1);
   });
+  it("case has duplicated in s & s", () => {
+    const arg = [[1], [2, 2], [3]];
+    const res = getCombination({ f: arg[0], s: arg[1], l: arg[2] });
+    expect(res).toContainEqual([1, 2, 3]);
+    expect(res.length).toStrictEqual(1);
+  });
 });
