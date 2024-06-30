70fdc802f308cfedace45aaf9ac40aab0b96f600
Kota <yuzukh0211@gmail.com>
2024-06-30 20:40:18 +0900

    [Green] 1 patern

--- front/src/app/utils/getCombination.ts
+++ front/src/app/utils/getCombination.ts
@@ -5,7 +5,15 @@ interface getCombinationArg {
 }
 
 const getCombination = ({ f, s, l }: getCombinationArg): number[][] => {
-  return [];
+  let res = [];
+  for (const i1 of f) {
+    for (const i2 of s) {
+      for (const i3 of l) {
+        res.push([i1, i2, i3]);
+      }
+    }
+  }
+  return res;
 };
 
 export default getCombination;
