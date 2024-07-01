05c511acb177e0ab1222de052c1df4c76a97e34e
Kota <yuzukh0211@gmail.com>
2024-07-01 23:28:49 +0900

    [Green] return permutation without duplicated  if args has first & second

--- front/src/app/utils/getPermutation.ts
+++ front/src/app/utils/getPermutation.ts
@@ -7,9 +7,10 @@ const getPermutaion = (
   if (typeof secondChoices == "undefined") {
     return [...firstWithoutDuplicated.map((el) => [el])];
   }
+  const secondWithoutDuplicated = [...new Set(secondChoices)];
   let res: number[][] = [];
-  for (const i1 of firstChoices) {
-    for (const i2 of secondChoices) {
+  for (const i1 of firstWithoutDuplicated) {
+    for (const i2 of secondWithoutDuplicated) {
       if (i1 === i2) {
         continue;
       }
