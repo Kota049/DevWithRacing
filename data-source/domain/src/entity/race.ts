2d26e88c869d4ce9aaf3199b0c2afeb627109152
Kota <yuzukh0211@gmail.com>
2024-07-07 16:58:49 +0900

    make entity

---
+++ data-source/domain/src/entity/race.ts
@@ -0,0 +1,24 @@
+import { Id } from './user';
+
+export class Race {
+  constructor(
+    public id: Id,
+    public startAt: Date,
+    public name: string,
+    public horses: Horse[],
+  ) {}
+
+  getBettingDeadline(now: Date): boolean {
+    const deadline = 2 * 60 * 1000;
+    const diff = this.startAt.getTime() - now.getTime();
+    return diff > deadline;
+  }
+}
+
+export class Horse {
+  constructor(
+    public order: number,
+    public name: string,
+    public jockey: string,
+  ) {}
+}
