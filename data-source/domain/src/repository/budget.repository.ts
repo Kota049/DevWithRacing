675c9fb5015724b8d452c27828ffc91b7153c32f
Kota <yuzukh0211@gmail.com>
2024-07-07 16:50:51 +0900

    make betting service

--- data-source/domain/src/repository/budget.repository.ts
+++ data-source/domain/src/repository/budget.repository.ts
@@ -1,4 +1,7 @@
 import { Budget } from 'src/entity/budget';
 import { RepositoryInterface } from './repository';
+import { Id } from 'src/entity/user';
 
-export abstract class BudgetRepositoryInterface extends RepositoryInterface<Budget> {}
+export abstract class BudgetRepositoryInterface extends RepositoryInterface<Budget> {
+  abstract findOne(userId: Id): Promise<Budget>;
+}
---
+++ data-source/domain/src/service/betting.service.ts
@@ -0,0 +1,49 @@
+import { Betting, BettingDetail } from 'src/entity/betting';
+import { Budget } from 'src/entity/budget';
+import { BudgetHistory, BugetUpdater } from 'src/entity/budget.history';
+import { Id } from 'src/entity/user';
+import { BettingRepositoryInterface } from 'src/repository/betting.repository';
+import { BudgetHistoryRepositoryInterface } from 'src/repository/budget.history.repository';
+import { BudgetRepositoryInterface } from 'src/repository/budget.repository';
+
+export class BettingService {
+  constructor(
+    private readonly br: BettingRepositoryInterface,
+    private readonly bur: BudgetRepositoryInterface,
+    private readonly bhr: BudgetHistoryRepositoryInterface,
+  ) {}
+
+  async betting(
+    userId: Id,
+    raceId: Id,
+    btds: BettingDetail[],
+  ): Promise<Betting> {
+    const now = new Date();
+    const currentBudget = (await this.bur.findOne(userId)).budget;
+    const totalBettingAmount = btds
+      .map((btd) => btd.amount)
+      .reduce((prev, current) => prev + current, 0);
+    if (currentBudget < totalBettingAmount) {
+      return Promise.reject('予算不足');
+    }
+
+    // update budget;
+    const budget = new Budget(currentBudget - totalBettingAmount, userId);
+    await this.bur.create(budget);
+
+    // create betting;
+    const betting = new Betting(userId, raceId, totalBettingAmount, btds);
+    const createdBettng = await this.br.create(betting);
+
+    // create budgetHistory;
+    const bettingHistory = new BudgetHistory(
+      userId,
+      totalBettingAmount * -1,
+      now,
+      BugetUpdater.BUY,
+    );
+    await this.bhr.create(bettingHistory);
+
+    return Promise.resolve(createdBettng);
+  }
+}
