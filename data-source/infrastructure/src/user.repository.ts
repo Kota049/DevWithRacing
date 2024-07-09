7b9fe3cd06c2f28bc53745872291f7830b7d3ca1
Kota <yuzukh0211@gmail.com>
2024-07-10 00:18:19 +0900

    make user repository

--- data-source/infrastructure/src/user.repository.ts
+++ data-source/infrastructure/src/user.repository.ts
@@ -1,6 +1,34 @@
-import { Id, User } from 'src/entity/user';
-import { RepositoryInterface } from './repository';
+import { Id, User } from '@domain/entity/user';
+import {
+  CreateUser,
+  UserRepositoryInterface,
+} from '@domain/repository/user.repository';
+import { PrismaService } from './prisma.service';
 
-export abstract class UserRepositoryInterface extends RepositoryInterface<User> {
-  abstract findOne(userId: Id): Promise<User>;
+export abstract class UserRepository implements UserRepositoryInterface {
+  constructor(private readonly prisma: PrismaService) {}
+  async create(t: CreateUser): Promise<User> {
+    const user = await this.prisma.user.create({
+      data: {
+        leverage: t.leverage,
+      },
+    });
+    const res = new User(new Id(user.id), user.leverage);
+    return Promise.resolve(res);
+  }
+  async update(t: User): Promise<User> {
+    const user = await this.prisma.user.update({
+      where: { id: t.userId.value },
+      data: { leverage: t.leverage },
+    });
+    const res = new User(new Id(user.id), user.leverage);
+    return Promise.resolve(res);
+  }
+  async findOne(userId: Id): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId.value },
+    });
+    const res = new User(new Id(user.id), user.leverage);
+    return Promise.resolve(res);
+  }
 }
